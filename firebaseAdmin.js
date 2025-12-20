a4139a4b562ac6a61dc8a3d2788ee46ed511cca0
Shriniwas <shriniwas@Shriniwass-MacBook-Air.local>
2025-12-20 11:38:24 +0530

    firebase configure

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,17 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp({
-  credential: admin.credential.cert(require("./firebase-service.json"))
-});
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("❌ FIREBASE_SERVICE_ACCOUNT env variable is missing");
+}
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
