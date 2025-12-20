fab0c6b1a383874b9ee82acd8a9da1eb7b566eb5
Shriniwas <shriniwas@Shriniwass-MacBook-Air.local>
2025-12-20 11:03:52 +0530

    our amenties margix and heading fix for mobile device

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,13 +1,7 @@
 const admin = require("firebase-admin");
 
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    process.env.FIREBASE_SERVICE_ACCOUNT
-  );
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
+admin.initializeApp({
+  credential: admin.credential.cert(require("./firebase-service.json"))
+});
 
 module.exports = admin;
